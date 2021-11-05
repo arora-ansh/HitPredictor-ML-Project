27b671cdbae1b56d1baaa155f6fb0455652c3055
Tushar Mohan <tushar.mohan2001@gmail.com>
2021-11-05 14:17:59 +0530

    script + data

--- data_extraction/lyrics.py
+++ data_extraction/lyrics.py
@@ -12,8 +12,8 @@ writer = csv.writer(f)
 # JSON API KEY from https://developers.google.com/custom-search/v1/overview Search Engine ID from https://cse.google.com/cse/create/new, website to search mei https://genius.com/
 # pass the GCS_API_KEY = AIzaSyAyrJ0ljh-58kXH0YX1eXo_DEdWR5axgqA, GCS_ENGINE_ID = 286d7776a1820cb5c
 # Tushar API key =  AIzaSyBJcu294CLp0MNFmNDmNHh35t4-qTjcuxU , GCS_ENGINE_ID = 7c91ebb5c3c3002e6
-# extract_lyrics = SongLyrics("AIzaSyAyrJ0ljh-58kXH0YX1eXo_DEdWR5axgqA","286d7776a1820cb5c") ## Ansh
-extract_lyrics = SongLyrics("AIzaSyBJcu294CLp0MNFmNDmNHh35t4-qTjcuxU","7c91ebb5c3c3002e6") ## Tushar
+extract_lyrics = SongLyrics("AIzaSyAyrJ0ljh-58kXH0YX1eXo_DEdWR5axgqA","286d7776a1820cb5c") ## Ansh
+# extract_lyrics = SongLyrics("AIzaSyBJcu294CLp0MNFmNDmNHh35t4-qTjcuxU","7c91ebb5c3c3002e6") ## Tushar
 i = 0
 df = pd.read_csv('../datasets/lyrics.csv')
 
@@ -21,4 +21,11 @@ for j in range(df.shape[0], m):
     t = data.iloc[j]["Track"] + " " + data.iloc[j]["Artist"]
     print(t,end = "\r")
     # print(extract_lyrics.get_lyrics(t))
-    writer.writerow([data.iloc[j]['Id'], t, extract_lyrics.get_lyrics(t)['lyrics']])
+    try:
+        writer.writerow([data.iloc[j]['Id'], t, extract_lyrics.get_lyrics(t)['lyrics']])
+    except Exception as e:
+        e = eval(str(e))
+        if(e['error']=='No results found'):
+            writer.writerow([data.iloc[j]['Id'], t, 'NA'])
+            continue
+        writer.writerow([data.iloc[j]['Id'], t, extract_lyrics.get_lyrics(t)['lyrics']])
