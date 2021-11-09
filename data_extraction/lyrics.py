a665ecf8d7073ae5307b245baf97de0967f2710d
Tushar Mohan <tushar.mohan2001@gmail.com>
2021-11-09 19:03:27 +0530

    lyrics

--- data_extraction/lyrics.py
+++ data_extraction/lyrics.py
@@ -14,8 +14,8 @@ writer = csv.writer(f)
 # Tushar API key =  AIzaSyBJcu294CLp0MNFmNDmNHh35t4-qTjcuxU , GCS_ENGINE_ID = 7c91ebb5c3c3002e6
 # extract_lyrics = SongLyrics("AIzaSyAyrJ0ljh-58kXH0YX1eXo_DEdWR5axgqA","286d7776a1820cb5c") ## Ansh
 # extract_lyrics = SongLyrics("AIzaSyBJcu294CLp0MNFmNDmNHh35t4-qTjcuxU","7c91ebb5c3c3002e6") ## Tushar
-extract_lyrics = SongLyrics("AIzaSyCKFsOTtgykRETOVq0OrF0jo4pu3iFw09o","f4943948815e35bda") ## Jishnu
-# extract_lyrics = SongLyrics("AIzaSyBkyvj7k0VuvwIL0-THk51JYNjEO1FC-Ok","afc1b54eeece36eee") ##Nandika
+# extract_lyrics = SongLyrics("AIzaSyCKFsOTtgykRETOVq0OrF0jo4pu3iFw09o","f4943948815e35bda") ## Jishnu
+extract_lyrics = SongLyrics("AIzaSyBkyvj7k0VuvwIL0-THk51JYNjEO1FC-Ok","afc1b54eeece36eee") ##Nandika
 i = 0
 df = pd.read_csv('../datasets/lyrics.csv')
 
